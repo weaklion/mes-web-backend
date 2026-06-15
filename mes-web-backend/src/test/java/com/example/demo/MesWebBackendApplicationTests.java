9d009bfc08f9ff4d09ee70be156d0ff18b2ad536
Lee gyu han <weaklion@naver.com>
2026-06-15 19:29:50 +0900

    project init

---
+++ mes-web-backend/src/test/java/com/example/demo/MesWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
